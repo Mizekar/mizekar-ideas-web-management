b12f1aca8bbd5f7bdbf054ab29204911932c6fb3
Mojtaba <mjr.rajabzadeh@gmail.com>
2019-01-30 12:41:27 +0330

    graphic improve...

--- src/containers/DefaultLayout/DefaultHeader.js
+++ src/containers/DefaultLayout/DefaultHeader.js
@@ -1,35 +1,35 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { Badge, DropdownItem, DropdownMenu, DropdownToggle, Nav, NavItem, NavLink } from 'reactstrap';
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import {Badge, Button, DropdownItem, DropdownMenu, DropdownToggle, Nav, NavItem, NavLink} from 'reactstrap';
 import PropTypes from 'prop-types';
 
-import { AppAsideToggler, AppHeaderDropdown, AppNavbarBrand, AppSidebarToggler } from '@coreui/react';
+import {AppAsideToggler, AppHeaderDropdown, AppNavbarBrand, AppSidebarToggler} from '@coreui/react';
 import logo from '../../assets/img/brand/logo.png'
 import sygnet from '../../assets/img/brand/sygnet.svg'
 
 const propTypes = {
-  children: PropTypes.node,
+    children: PropTypes.node,
 };
 
 const defaultProps = {};
 
 class DefaultHeader extends Component {
-  render() {
+    render() {
 
-    // eslint-disable-next-line
-    const { children, ...attributes } = this.props;
+        // eslint-disable-next-line
+        const {children, ...attributes} = this.props;
 
-    return (
-      <React.Fragment>
-        <AppSidebarToggler className="d-lg-none" display="md" mobile />
-        <AppNavbarBrand
-          full={{ src: logo, width: 89, height: 60, alt: 'سوژه' }}
-          minimized={{ src: sygnet, width: 30, height: 30, alt: 'سوژه' }}
-        />
-        <AppSidebarToggler className="d-md-down-none" display="lg" />
+        return (
+            <React.Fragment>
+                <AppSidebarToggler className="d-lg-none" display="md" mobile/>
+                <AppNavbarBrand
+                    full={{src: logo, width: 100, height: 44, alt: 'سوژه'}}
+                    minimized={{src: sygnet, width: 30, height: 30, alt: 'سوژه'}}
+                />
+                <AppSidebarToggler className="d-md-down-none" display="lg"/>
 
-        <Nav className="d-md-down-none" navbar>
-        {/*  <NavItem className="px-3">
+                <Nav className="d-md-down-none" navbar>
+                    {/*  <NavItem className="px-3">
             <NavLink href="/">داشبورد</NavLink>
           </NavItem>
           <NavItem className="px-3">
@@ -38,29 +38,36 @@ class DefaultHeader extends Component {
           <NavItem className="px-3">
             <NavLink href="#">تنظیمات</NavLink>
           </NavItem>*/}
-        </Nav>
-        <Nav className="mr-auto" navbar>
-          <NavItem className="d-md-down-none">
-            {/*<NavLink href="#"><i className="icon-bell"></i><Badge pill color="danger">5</Badge></NavLink>*/}
-          </NavItem>
-          <AppHeaderDropdown direction="down">
-            <DropdownToggle nav>
-              <img src={'../../assets/img/avatars/default.png'} className="img-avatar" alt="" />
-            </DropdownToggle>
-            <DropdownMenu right style={{ right: 'auto' }}>
-              <DropdownItem header tag="div" className="text-center">حساب کاربری</DropdownItem>
-              <DropdownItem><i className="fa fa-user-circle"></i>ویرایش پروفایل</DropdownItem>
-              <DropdownItem><i className="fa fa-key"></i> تغییر کلمه عبور</DropdownItem>
-              <DropdownItem><i className="fa fa-comments"></i> پیام ها<Badge color="warning">10</Badge></DropdownItem>
-              <DropdownItem onClick={e => this.props.onLogout(e)}><i className="fa fa-lock"></i> خروج از سامانه</DropdownItem>
-            </DropdownMenu>
-          </AppHeaderDropdown>
-        </Nav>
-        {/*<AppAsideToggler className="d-md-down-none" />*/}
-        {/*<AppAsideToggler className="d-lg-none" mobile />*/}
-      </React.Fragment>
-    );
-  }
+                </Nav>
+                <Nav className="mr-auto" navbar>
+                    <NavItem className="d-md-down-none">
+                        {/*<NavLink href="#"><i className="icon-bell"></i><Badge pill color="danger">5</Badge></NavLink>*/}
+                    </NavItem>
+                    <NavItem className="d-md-down-none">
+                        <Button className="btn btn-warning ml-4">5 پیام جدید</Button>
+                        <Button className="btn-header">تماس با مدیر</Button>
+                        <Button onClick={e => this.props.onLogout(e)} className="btn-header">خروج</Button>
+                    </NavItem>
+                    {/*<AppHeaderDropdown direction="down">
+                        <DropdownToggle nav>
+                            <img src={'../../assets/img/avatars/default.png'} className="img-avatar" alt=""/>
+                        </DropdownToggle>
+                        <DropdownMenu right style={{right: 'auto'}}>
+                            <DropdownItem header tag="div" className="text-center">حساب کاربری</DropdownItem>
+                            <DropdownItem><i className="fa fa-user-circle"></i>ویرایش پروفایل</DropdownItem>
+                            <DropdownItem><i className="fa fa-key"></i> تغییر کلمه عبور</DropdownItem>
+                            <DropdownItem><i className="fa fa-comments"></i> پیام ها<Badge
+                                color="warning">10</Badge></DropdownItem>
+                            <DropdownItem ><i className="fa fa-lock"></i> خروج از
+                                سامانه</DropdownItem>
+                        </DropdownMenu>
+                    </AppHeaderDropdown>*/}
+                </Nav>
+                {/*<AppAsideToggler className="d-md-down-none" />*/}
+                {/*<AppAsideToggler className="d-lg-none" mobile />*/}
+            </React.Fragment>
+        );
+    }
 }
 
 DefaultHeader.propTypes = propTypes;
